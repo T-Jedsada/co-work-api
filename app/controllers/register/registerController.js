409f0bbd1bc895e59118128525023471291712ae
johnjohncater <chachat_2015@hotmail.com>
2018-02-22 15:45:35 +0700

    manage api to MVC

--- app/controllers/register/registerController.js
+++ app/controllers/register/registerController.js
@@ -1,13 +1,52 @@
 var mongojs = require('mongojs');
+var bcrypt = require('bcrypt');
 
 var database = mongojs('mongodb://root:root@ds245228.mlab.com:45228/cowork', ['users']);
 
-// Display list of all Authors.
-exports.list = function(req, res, next) {
+/* List data in database */
+exports.index = function(req, res, next) {
     database.users.find(function(err, users){
         if(err){
             res.send(err);
         }
         res.json(users);
+    });
+};
+
+/* Save Register */
+exports.store = function(req, res, next) {
+    var user = req.body;
+    if(!user.name || !user.email || !user.password || !user.image){
+        res.status(400);
+        res.json({
+            "success": "fail",
+            "massage": "Eroor 400"
+        });
+    } else {
+        bcrypt.hash(user.password, 10, function(err, hash) {
+            user.password = hash;
+        });
+        database.users.save(user, function(err, user){
+            if(err){
+                res.send(err);
+            }
+            res.json({
+                "success": "true",
+                "data": user
+            });
+        });
+    }
+};
+
+/* Delete User Register */
+exports.delete = function(req, res, next) {
+    database.users.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, user){
+        if(err){
+            res.send(err);
+        }
+        res.json({
+            "success": "true",
+            "data": user
+        });
     });
 };
--- routes/api.js
+++ routes/api.js
@@ -1,45 +1,11 @@
 var express = require('express');
-var mongojs = require('mongojs');
 var router = express.Router();
-var database = mongojs('mongodb://root:root@ds245228.mlab.com:45228/cowork', ['users']);
-
-
 
 var register_controller = require('../app/controllers/register/registerController');
-router.get('/register', register_controller.list);
 
-
-/* List data in database */
-router.get('/', function(req, res, next) {
-    database.users.find(function(err, users){
-        if(err){
-            res.send(err);
-        }
-        res.json(users);
-    });
-    //res.send('ffff');
-});
-
-//Save Register
-router.post('/', function(req, res, next){
-    var user = req.body;
-    if(!user.name || !user.email || !user.password || !user.image){
-        res.status(400);
-        res.json({
-            "success": "fail",
-            "massage": "Eroor 400"
-        });
-    } else {
-        database.users.save(user, function(err, user){
-            if(err){
-                res.send(err);
-            }
-            res.json({
-                "success": "true",
-                "data": user
-            });
-        });
-    }
-});
+/* Api for register */
+router.get('/register', register_controller.index);
+router.post('/register', register_controller.store);
+router.delete('/register/delete/:id', register_controller.delete);
 
 module.exports = router;
