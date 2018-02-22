9111b2051436fccbd59b8d3f6f1bfb142afaaccb
johnjohncater <chachat_2015@hotmail.com>
2018-02-22 15:48:28 +0700

    update manage api to MVC

--- routes/api.js
+++ routes/api.js
@@ -8,4 +8,5 @@ router.get('/register', register_controller.index);
 router.post('/register', register_controller.store);
 router.delete('/register/delete/:id', register_controller.delete);
 
+//fvdf
 module.exports = router;
