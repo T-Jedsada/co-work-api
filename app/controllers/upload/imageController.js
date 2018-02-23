d1b8142e8f16e107f5ed5cbb43167adb4055bc99
johnjohncater <chachat_2015@hotmail.com>
2018-02-23 11:31:03 +0700

    can upload images

--- app/controllers/upload/imageController.js
+++ app/controllers/upload/imageController.js
@@ -1,5 +1,14 @@
-
+var multer = require('multer');
 var image_profile = "public/upload/images/profile";
+
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, image_profile);
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
 
 /* get image name */
 exports.index = function(req, res, next) {
@@ -8,12 +17,25 @@ exports.index = function(req, res, next) {
 
 /* upload and save image name */
 exports.upload = function(req, res, next) {
-    if (!req.files){
-        res.status(400);
-        res.json({
-            "success": false,
-            "massage": "No files were uploaded."
-        });
-    }
+    var upload = multer({ storage: storage }).single('image');
+    upload(req, res, function(err) {
+        if (err) {
+            res.json({
+                success : false,
+                "massage": "Something went wrong!"
+            });
+        }
 
+        if(req.file === undefined){
+            res.json({
+                success : false,
+                "massage": "Error: No File Selected!"
+            });
+        }
+
+        res.json({
+            success : true,
+            "massage": "File uploaded sucessfully!"
+        });
+    });
 };
