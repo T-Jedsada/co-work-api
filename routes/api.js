50f8144bd24b789f6d28b1362190b051b21abc03
johnjohncater <chachat_2015@hotmail.com>
2018-02-28 13:58:56 +0700

    commit success

--- routes/api.js
+++ routes/api.js
@@ -4,12 +4,14 @@ var multer = require('multer');
 var upload = multer();
 
 var register_controller = require('../app/controllers/api/register/register_controller');
+var register_provider_controller = require('../app/controllers/api/register/register_provider_controller');
 var image_controller = require('../app/controllers/api/upload/upload_images_controller');
 var send_email_controller = require('../app/controllers/api/send_email/send_email_controller');
 
 /* Api for register */
 router.get('/register', register_controller.index);
-router.post('/register', register_controller.store);
+router.post('/register', upload.any(), register_controller.store);
+router.post('/register/provider',  register_provider_controller.store);
 router.get('/register/verify/:id', register_controller.verify);
 router.delete('/register/delete/:id', register_controller.delete);
 router.delete('/register/delete-overall', register_controller.delete_overall);
