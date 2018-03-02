3963867fbd5e0045bf783215c025b43b334691a4
johnjohncater <chachat_2015@hotmail.com>
2018-03-02 16:08:23 +0700

    update register any

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer();
+var midleware = multer();
 
 var register_controller = require('../app/controllers/api/register/register_controller');
 var forgot_password_controller = require('../app/controllers/api/register/forgot_password/forgot_password_controller');
@@ -12,7 +12,7 @@ var change_password_controller = require('../app/controllers/api/send_email/chan
 
 /* Api for register */
 router.get('/register', register_controller.index);
-router.post('/register', upload.fields([]),register_controller.store);
+router.post('/register', midleware.any(),register_controller.store);
 router.post('/register/provider',  register_provider_controller.store);
 router.get('/register/verify/:id', register_controller.verify);
 router.delete('/register/delete/:id', register_controller.delete);
@@ -20,7 +20,7 @@ router.delete('/register/delete-overall', register_controller.delete_overall);
 
 /* Api for forgot password */
 //router.post('/register/forgot-password', forgot_password_controller.forgot_password);
-router.post('/register/forgot-password', upload.fields([]), forgot_password_controller.forgot_password);
+router.post('/register/forgot-password', midleware.any(), forgot_password_controller.forgot_password);
 
 /* Api for file image upload */
 router.post('/upload-image', upload_controller.upload);
