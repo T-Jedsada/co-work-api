34b0613c9ea8c8c5d1ba73c84fccf03ec330a404
johnjohncater <chachat_2015@hotmail.com>
2018-03-06 04:15:42 +0700

    chane password basic

--- app/controllers/api/register/forgot_password/change_password_controller.js
+++ app/controllers/api/register/forgot_password/change_password_controller.js
@@ -1,6 +1,27 @@
+require('dotenv').config();
+var mongojs = require('mongojs');
+var bcrypt = require('bcrypt');
+var base_response = require('../../base_controller');
 
+var database = mongojs(process.env.CONFIG_DATABASE,[process.env.DB_TABLE_USERS]);
 
 /* List data in database */
 exports.index = function(req, res, next) {
-    return res.json(base_response.error('this test'))
+    var user = {};
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    if(!user.email || !user.password){
+        res.status(400);
+        return res.json(base_response.error('The details are not complete.'));
+    }
+    bcrypt.hash(user.password, 10, function (err, hash) {
+        user.password = hash;
+    });
+    database.users.update({_id: mongojs.ObjectId(req.params.id)}, { $set: { password: user.password } }, function (err, user) {
+        if (err) {
+            return res.json(base_response.error('Can not save change password'));
+        }
+        return res.json(base_response.success(user));
+    });
 };
