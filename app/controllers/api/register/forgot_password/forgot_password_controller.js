9f38dfc70d337781c944fafce7f09a6b4c32f34b
johnjohncater <chachat_2015@hotmail.com>
2018-03-06 23:48:08 +0700

    add check email address

--- app/controllers/api/register/forgot_password/forgot_password_controller.js
+++ app/controllers/api/register/forgot_password/forgot_password_controller.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 var mongojs = require('mongojs');
+var is_email = require("email-validator");
 var base_response = require('../../../base_controller');
 
 var database = mongojs(process.env.CONFIG_DATABASE,[process.env.DB_TABLE_USERS]);
 
-/* check email */
 exports.forgot_password = function(req, res, next) {
-    var user = req.body;
-    database.users.findOne({email: user.email}, function(err, user) {
+    var email = req.body.email;
+    if (!email){
+        return res.json(base_response.error('The details are not complete.'));
+    }
+    if (is_email.validate(email) === false){
+        return res.json(base_response.error('Email not have @address.'));
+    }
+    database.users.findOne({email: email}, function(err, user) {
         if (!user){
             return res.json(base_response.error('This email do not sing up')) ;
         }
