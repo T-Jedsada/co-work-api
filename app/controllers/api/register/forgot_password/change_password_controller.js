f3892fa88d831f41ab187e9887172987432badf2
johnjohncater <chachat_2015@hotmail.com>
2018-03-06 11:08:45 +0700

    reset

--- app/controllers/api/register/forgot_password/change_password_controller.js
+++ app/controllers/api/register/forgot_password/change_password_controller.js
@@ -2,5 +2,5 @@
 
 /* List data in database */
 exports.index = function(req, res, next) {
-    return res.json(base_response.error('this test'))
+    return res.json(base_response.error('this test '))
 };
