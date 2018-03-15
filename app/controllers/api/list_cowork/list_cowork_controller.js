5bc93aaf8c4207533c850175f8c3436a854f98ee
johnjohncarter <chachat_2015@hotmail.com>
2018-03-15 18:26:14 +0700

    list co-work

--- app/controllers/api/list_cowork/list_cowork_controller.js
+++ app/controllers/api/list_cowork/list_cowork_controller.js
@@ -2,17 +2,6 @@ var base_response = require('../../base_controller');
 var mongoose = require ("mongoose");
 
 exports.store = function(req, res, next) {
-    var uristring = process.env.CONFIG_DATABASE,[process.env.DB_TABLE_USERS];
-    mongoose.connect(uristring, function (err, res) {
-        if (err) {
-            console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-        } else {
-            console.log ('Succeeded connected to: ' + uristring);
-        }
-    });
-
-    var list_cowork_form = req.body;
-    var list_cowork = {};
 
     return res.json(base_response.success(list_cowork_form.status.name));
 };
